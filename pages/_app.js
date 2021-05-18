452393e7a9778636a1225aecb7b5f6a8b5366019
Elad <Elad.Karni@Gmail.com>
2021-05-18 01:52:30 -0400

    moving css variables to a seperate file

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import '../styles/colors.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
