b9b0f9b604f16b513d02c2c1a2edc472682cc793
Elad <Elad.Karni@Gmail.com>
2021-05-21 00:17:04 -0400

    form test number 2. redirect removed

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Styles from './Contact.module.scss'
 
 function Contact() {
     const [success, setSuccess] = useState(false);
@@ -10,24 +11,26 @@ function Contact() {
     }, []);
 
     return (
-        <div maxWidth="sm">
+        <div className={Styles.Contact}>
             <h2>CONTACT</h2>
             {success && (
                 <p style={{ color: "green" }}>Thanks for your message! </p>
             )}
             <div>
                 <form 
-                    name="contact" 
-                    method="POST" 
-                    action="/contact/?success=true"
+                    name="ContactMe" 
+                    method="POST"
                     data-netlify="true" 
                 >
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
-                        <label>Name <input type="text" name="name" /></label>
+                        <label>Your Name: <input type="text" name="name"/></label>
                     </p>
                     <p>
-                        <label>Email <input type="email" name="email" /></label>
+                        <label>Your Email: <input type="email" name="email"/></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
                     </p>
                     <p>
                         <button type="submit">Send</button>
