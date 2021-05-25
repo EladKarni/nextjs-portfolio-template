c084871d5f6dca288f9841b26bb3773438efb734
kaustubh <kaustubh229@gmail.com>
2021-05-26 00:04:04 +0530

    Animation fixed

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,46 @@
-import React, { useEffect } from 'react';
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import AOS from 'aos'
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import Header from '../components/Header/Header'
+import Header from "../components/Header/Header";
 
-const About = dynamic(() => import('../components/About/About'))
-const Timeline = dynamic(() => import('../components/Timeline/Timeline'))
-const Projects = dynamic(() => import('../components/Projects/Projects'))
+const About = dynamic(() => import("../components/About/About"));
+const Timeline = dynamic(() => import("../components/Timeline/Timeline"));
+const Projects = dynamic(() => import("../components/Projects/Projects"));
 
-
-
-import Styles from '../styles/index.module.scss'
+import Styles from "../styles/index.module.scss";
 export default function Home() {
-
   useEffect(() => {
-    if (localStorage.getItem('mode') == 'Dark') {
-      document.documentElement.classList.toggle('dark-mode')
-      const checkbox = document.getElementById('mode-switch')
-      checkbox.checked = true
+    if (localStorage.getItem("mode") == "Dark") {
+      document.documentElement.classList.toggle("dark-mode");
+      const checkbox = document.getElementById("mode-switch");
+      checkbox.checked = true;
     }
 
     AOS.init({
       offset: 200,
       duration: 600,
-      easing: 'ease',
+      easing: "ease",
       delay: 100,
     });
-  }, [])
+  }, []);
 
   return (
     <div className={Styles.App}>
       <Head>
         <title>Elad's Resume Website</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Elad's resume website that was built using an open source template that I developed with Next.JS" />
+        <meta
+          name="description"
+          content="Elad's resume website that was built using an open source template that I developed with Next.JS"
+        />
       </Head>
-      <Header/>
-      <About/>
+      <Header />
+      <About />
       <Timeline />
       <Projects />
     </div>
-  )
+  );
 }
