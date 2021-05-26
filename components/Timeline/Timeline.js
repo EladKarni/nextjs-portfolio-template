b8b73c1bba4542dd5191fc583a61158ca1a21c4d
Elad <Elad.Karni@Gmail.com>
2021-05-26 11:24:52 -0400

    fixed timeline merge error

--- components/Timeline/Timeline.js
+++ components/Timeline/Timeline.js
@@ -4,20 +4,22 @@ import { timelineData as jobs } from "../../content";
 import Styles from "./Timeline.module.scss";
 
 const Timeline = () => (
-    <div className={Styles.Timeline}>
-        <h2>Job Timeline</h2>
-        <div className={Styles.TimelineContainer}>
-            {jobs.map((data, idx) => (
-                <div className={Styles.TimelineItem} key={idx}>
-                    <div className={Styles.TimelineContent}>
-                        <h4>{data.company}</h4>
-                        <h5>{data.title}</h5>
-                        <time>{data.date}</time>
-                        <p>{data.text}</p>
-                        <span className={Styles.Circle} />
-                    </div>
-                </div>
-            ))}
+  <div className={Styles.Timeline}>
+    <h2>Job Timeline</h2>
+    <div className={Styles.TimelineContainer}>
+      {jobs.map((data, idx) => (
+        <div
+          data-aos={idx % 2 == 0 ? "fade-left" : "fade-right"}
+          className={Styles.TimelineItem}
+          key={idx}
+        >
+          <div className={Styles.TimelineContent}>
+            <h4>{data.company}</h4>
+            <h5>{data.title}</h5>
+            <time>{data.date}</time>
+            <p>{data.text}</p>
+            <span className={Styles.Circle} />
+          </div>
         </div>
       ))}
     </div>
