6ce0dbad83d7dfbbcd7ae1f1d40ee93c0af15d75
Elad <Elad.Karni@Gmail.com>
2021-05-27 13:15:06 -0400

    updated BTTB and header clicking order for partical interaction and button functionality

--- components/BackToTopBtn/BackToTopBtn.js
+++ components/BackToTopBtn/BackToTopBtn.js
@@ -12,7 +12,7 @@ function BackToTopBtn() {
 
     return (
         <div className={Styles.BackToTopBtn} >
-            <img id="not-dark" onClick={scrollTo} alt="ScrollUp" title="ScrollUp" width="128" height="128" src="/thor.svg" />
+            <a href="#"><img id="not-dark" alt="ScrollUp" title="ScrollUp" width="128" height="128" src="/thor.svg" /></a>
         </div>
     )
 }
